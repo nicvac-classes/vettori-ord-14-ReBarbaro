a2c17298bf45da5693ca5f726d9f06464d716b83
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2025-04-02 14:46:01 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+
+
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n, i, t, j;
+
+    cout << "quanti sono gli studenti ?" << endl;
+    cin >> n;
+    string nomi[n];
+    int voti[n];
+
+    i = 0;
+    while (i < n) {
+        cout << "quale è il" << i + 1 << "nome?" << endl;
+        cin >> nomi[i];
+        cout << "quale è il" << i + 1 << "voto?" << endl;
+        cin >> voti[i];
+        i = i + 1;
+    }
+    string c;
+
+    i = 0;
+    j = 0;
+    t = 0;
+    while (i < n) {
+        j = 0;
+        while (j <= n - 2 - i) {
+            if (voti[j] > voti[j + 1]) {
+                t = voti[j];
+                voti[j] = voti[j + 1];
+                voti[j + 1] = t;
+                c = nomi[j];
+                nomi[j] = nomi[j + 1];
+                nomi[j + 1] = c;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << "il" << i + 1 << "nome è" << nomi[i] << "il corrispondente voto è" << voti[i] << endl;
+        i = i + 1;
+    }
+    
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
